bc95787f89e4cf481268257015de2f8119eaa4df
isambi1 <56687822+isambi1@users.noreply.github.com>
2019-11-12 11:53:07 -0500

    Fixed flash no showing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
     @user = User.new(params.require(:user).permit(:name, :email, :password, :password_confirmation))
     if @user.save
       # Handle a successful save.
+      flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
       render 'new'
